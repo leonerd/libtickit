e683538047fcf8d59ff1d3be2776802929a9cc0b
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2018-04-09 23:47:47 +0100

    Don't attempt to select() on invalid fds (thanks Steve Langasek) (LP#1744933)

--- src/term.c
+++ src/term.c
@@ -682,6 +682,9 @@ void tickit_term_input_wait_msec(TickitTerm *tt, long msec)
   FD_ZERO(&readfds);
 
   int fd = termkey_get_fd(tk);
+  if (fd < 0 || fd >= FD_SETSIZE)
+    return;
+
   FD_SET(fd, &readfds);
   int ret = select(fd + 1, &readfds, NULL, NULL, msec > -1 ? &timeout : NULL);
 
