96d100e2f18f7f8026a40955c9c59b1875551180
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2019-02-07 16:46:35 +0000

    Unit-test (but don't actually document yet) tickit_watch_io_read()

---
+++ t/51tickit-io.c
@@ -0,0 +1,46 @@
+#include "tickit.h"
+#include "tickit-mockterm.h"
+#include "taplib.h"
+
+#include <unistd.h>
+
+static int on_call_incr(Tickit *t, TickitEventFlags flags, void *user)
+{
+  if(flags & TICKIT_EV_FIRE) {
+    int *ip = user;
+    (*ip)++;
+
+    tickit_stop(t);
+  }
+
+  return 1;
+}
+
+int main(int argc, char *argv[])
+{
+  Tickit *t = tickit_new_for_term(tickit_mockterm_new(25, 80));
+
+  {
+    int fds[2];
+    if(pipe(fds) != 0) {
+      perror("pipe");
+      exit(1);
+    }
+
+    int counter = 0;
+
+    void *watch = tickit_watch_io_read(t, fds[0], 0, &on_call_incr, &counter);
+
+    write(fds[1], "OUT\n", 4);
+
+    tickit_run(t);
+
+    is_int(counter, 1, "tickit_watch_io_read invokes callback");
+
+    tickit_watch_cancel(t, watch);
+
+    /* TODO: fds[0] is still readready currently but lets just throw it away */
+  }
+
+  return exit_status();
+}
