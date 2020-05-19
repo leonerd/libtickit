a39f08d4a3efe09a343c197cb953a956f6a451a4
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2020-05-20 00:03:44 +0100

    Skip the tickit_term_build .ti_getstr hook unit test if platform doesn't have terminfo for TERM=linux

--- t/18term-builder.c
+++ t/18term-builder.c
@@ -23,26 +23,35 @@ int main(int argc, char *argv[])
 {
   // getstr override
   {
+    /* We need a termtype that isn't xterm, but that will actually load.
+     * Rather than relying on this to actually load, we'll just skip the test
+     * if it doesn't
+     */
     struct TickitTermBuilder builder = {
       .termtype = "linux",
       .ti_hook = &(const struct TickitTerminfoHook){ .getstr = ti_getstr },
     };
     TickitTerm *tt = tickit_term_build(&builder);
-    ok(!!tt, "tickit_term_build for .termtype, .ti_hook");
+    if(tt) {
+      pass("tickit_term_build for .termtype, .ti_hook");
 
-    char buffer[1024] = { 0 };
+      char buffer[1024] = { 0 };
 
-    tickit_term_set_output_func(tt, output, buffer);
-    tickit_term_set_output_buffer(tt, 4096);
+      tickit_term_set_output_func(tt, output, buffer);
+      tickit_term_set_output_buffer(tt, 4096);
 
-    buffer[0] = 0;
+      buffer[0] = 0;
 
-    tickit_term_goto(tt, 4, 6);
-    tickit_term_flush(tt);
+      tickit_term_goto(tt, 4, 6);
+      tickit_term_flush(tt);
 
-    is_str_escape(buffer, "GOTO(4,6)", "buffer after goto for built term");
+      is_str_escape(buffer, "GOTO(4,6)", "buffer after goto for built term");
 
-    tickit_term_unref(tt);
+      tickit_term_unref(tt);
+    }
+    else {
+      skip("no terminfo for TERM=linux");
+    }
   }
 
   return exit_status();
