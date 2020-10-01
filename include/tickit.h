cda1dd7f0ff32a96a6a5437a7d73c9f9765c2ccc
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2020-10-01 17:55:26 +0100

    Bumped version to 0.4.1; updated CHANGES notes

--- include/tickit.h
+++ include/tickit.h
@@ -26,7 +26,7 @@ extern "C" {
 
 #define TICKIT_VERSION_MAJOR 0
 #define TICKIT_VERSION_MINOR 4
-#define TICKIT_VERSION_PATCH 0
+#define TICKIT_VERSION_PATCH 1
 
 int tickit_version_major(void);
 int tickit_version_minor(void);
