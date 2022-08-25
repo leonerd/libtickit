028a880689a253f1bd6078182f13036d0b4dad0d
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2022-08-25 16:34:36 +0100

    Created an example program for showing all the current termctl values, including private ones

---
+++ examples/print-termctl.c
@@ -0,0 +1,73 @@
+#include "tickit.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void printctl(TickitTerm *tt, const char *name, int ctl)
+{
+  switch(tickit_term_ctltype(ctl)) {
+    case TICKIT_TYPE_NONE:
+      break;
+
+    case TICKIT_TYPE_BOOL:
+    {
+      int b;
+      tickit_term_getctl_int(tt, ctl, &b);
+      printf("  %-26s: %s\n", name, b ? "true" : "false");
+      break;
+    }
+
+    case TICKIT_TYPE_INT:
+    {
+      int i;
+      tickit_term_getctl_int(tt, ctl, &i);
+      printf("  %-26s: %d\n", name, i);
+      break;
+    }
+
+    // currently there are no controls of type STR or COLOUR
+    case TICKIT_TYPE_STR:
+    case TICKIT_TYPE_COLOUR:
+      break;
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  Tickit *t = tickit_new_stdtty();
+
+  TickitTerm *tt = tickit_get_term(t);
+  if(!tt) {
+    fprintf(stderr, "Cannot create TickitTerm - %s\n", strerror(errno));
+    return 1;
+  }
+
+  tickit_term_await_started_msec(tt, 100);
+
+  // First the common controls
+  printf("Common:\n");
+  for(int ctl = 1; ; ctl++) {
+    const char *name = tickit_term_ctlname(ctl);
+    if(!name)
+      break;
+
+    printctl(tt, name, ctl);
+  }
+
+  // Now the custom ones
+  printf("Driver %s\n", tickit_term_get_drivername(tt));
+  int range = tickit_term_get_driverctl_range(tt);
+  for(int ctl = range+1; range; ctl++) {
+    const char *name = tickit_term_ctlname(ctl);
+    if(!name)
+      break;
+
+    printctl(tt, name, ctl);
+  }
+
+  tickit_unref(t);
+
+  return 0;
+}
